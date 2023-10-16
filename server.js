4b41948d0cc21d2836543a62a968c6fc8d6cb01c
shlok0070 <shlok0070@gmail.com>
2023-10-17 02:14:42 +0530

    Cricket Dictionary Modified for all functionality

--- server.js
+++ server.js
@@ -1,58 +1,103 @@
 const express = require('express');
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
-const mysql = require('mysql2');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
-const port = 3000;
-
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'nodecomplete1',
-    database: 'node-complete'
-});
-
-connection.connect();
-
-
+app.use(cors()); 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
+const connection = mysql.createConnection({
+    host: 'localhost', 
+    user: 'root',      
+    password: 'nodecomplete1',      
+    database: 'node-complete'  
+});
 
+connection.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+// Add new player
+app.post('/players', (req, res) => {
+    const { name, team, role, batting_average, bowling_average, image_url } = req.body;
+    const query = `INSERT INTO cricket_players (name, team, role, batting_average, bowling_average, image_url) VALUES (?, ?, ?, ?, ?, ?)`;
+
+    connection.query(query, [name, team, role, batting_average, bowling_average, image_url], (err, results) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json({ message: 'Player added successfully', player: req.body });
+    });
+});
+
+
+
+// Update player
+app.put('/players/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, team, role, batting_average, bowling_average, image_url } = req.body;
+    const query = `UPDATE cricket_players SET name = ?, team = ?, role = ?, batting_average = ?, bowling_average = ?, image_url = ? WHERE id = ?`;
+
+    connection.query(query, [name, team, role, batting_average, bowling_average, image_url, id], (err, results) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json({ message: 'Player updated successfully', player: req.body });
+    });
+});
+
+
+
+
+// Fetch all players
 app.get('/players', (req, res) => {
     connection.query('SELECT * FROM cricket_players', (err, results) => {
-        if (err) throw err;
-        res.json(results);
+        if (err) return res.status(500).send(err);
+        res.status(200).json(results);
     });
 });
 
-
-
-app.post('/players', (req, res) => {
-    const { name, team, role, batting_average, bowling_average } = req.body;
-    const query = `INSERT INTO cricket_players (name, team, role, batting_average, bowling_average) VALUES (?, ?, ?, ?, ?)`;
-    
-    connection.query(query, [name, team, role, batting_average, bowling_average], (err, results) => {
-        if (err) throw err;
-        res.json({ message: 'Player added successfully', player: req.body });
-    });
-});
-
-
+// Search for players
 app.get('/players/search', (req, res) => {
     const name = req.query.name;
-    const query = `SELECT * FROM cricket_players WHERE name LIKE ?`;
+    const query = 'SELECT * FROM cricket_players WHERE name LIKE ?';
+    connection.query(query, [`%${name}%`], (err, results) => {
+        if (err) return res.status(500).send(err);
+        res.status(200).json(results);
+    });
+});
 
-    connection.query(query, ['%' + name + '%'], (err, results) => {
-        if (err) throw err;
-        res.json(results);
+// Fetch a single player
+app.get('/players/:id', (req, res) => {
+    const { id } = req.params;
+    const query = 'SELECT * FROM cricket_players WHERE id = ?';
+
+    connection.query(query, [id], (err, results) => {
+        if (err) {
+            console.error('Error fetching player by ID:', err);
+            return res.status(500).json({ message: 'Error fetching player by ID', error: err });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'Player not found' });
+        }
+
+        res.status(200).json(results[0]);  
     });
 });
 
 
+// Serve the HTML file for any other route including root
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
-app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+const PORT = 3000; 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
